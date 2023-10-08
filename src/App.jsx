effa21b5d45ff798f241d99f76a8eb8d0d8909d8
SafarovHusan200 <safarovhusan2000@gmail.com>
2023-10-08 18:42:33 +0500

    queryID

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,9 @@ export default function App() {
         body: JSON.stringify(cartItems),
       });
     } else {
-      telegram.sendData(JSON.stringify(cartItems));
+      telegram.sendData(
+        JSON.stringify({ products: cartItems, queryID: queryID })
+      );
     }
   }, [cartItems]);
 
