4b8f6c74a58c1d037d76a3238a4b24df954519e1
SafarovHusan200 <safarovhusan2000@gmail.com>
2023-10-08 20:02:37 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -63,12 +63,10 @@ export default function App() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(cartItems),
+        body: JSON.stringify({ products: cartItems, queryID: queryID }),
       });
     } else {
-      telegram.sendData(
-        JSON.stringify({ products: cartItems, queryID: queryID })
-      );
+      telegram.sendData(JSON.stringify(cartItems));
     }
   }, [cartItems]);
 
